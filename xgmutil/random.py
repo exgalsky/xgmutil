db4cf3997b73d9ff5ade6290c14bf7f9a4240a44
Shamik Ghosh <thequarkexpress@gmail.com>
2023-11-14 17:05:46 -0800

    New random module with RNG manager and stream

---
+++ xgmutil/random.py
@@ -0,0 +1,51 @@
+import jax.numpy as jnp 
+import jax.random as rdm 
+
+import xgmutil.stream as stream
+
+cmb_dict = {'cmb_tlm': 0, 'cmb_elm': 1, 'cmb_blm': 2}
+lens_dict = {'phi_lm': 3}
+# dust_list = [[4, 'dust_tlm'], [5, 'dust_elm'], [6, 'dust_blm']]
+# sync_list = [[7, 'sync_tlm'], [8, 'sync_elm'], [9, 'sync_blm']]
+ic_dict = {'ic_grid': 32}
+
+predefined_components = cmb_dict | lens_dict | ic_dict
+
+class RNG_component:
+    def __init__(self, component_name, stream_id, stream_key, **kwargs):
+        self.component     = component_name
+        self.component_id  = stream_id
+        self.component_key = stream_key
+        self.force_no_gpu  = kwargs.get('force_no_gpu',False)
+        self.nsub          = kwargs.get('nsub',1024**3)
+
+        self.stream = stream.Stream(force_no_gpu=self.force_no_gpu, seedkey=self.component_key, nsub=self.nsub)
+
+    def generate(self, **kwargs):
+        return self.stream.generate(**kwargs)
+
+class RNG_manager:
+    def __init__(self, seed = 57885161, n_components = 512):
+    # Mersenne exponents no 48. Ref: https://oeis.org/A000043 used as seed
+    # Do we hard code this here, or have it defined elsewhere and read here?
+
+        self.master_key  = rdm.PRNGKey(seed)
+        self.key_list    = rdm.split(self.master_key, num=n_components)
+        self.stream_no   = jnp.arange(0, n_components, dtype=jnp.int16)
+        self.stream_id   = [f"{seed}-{seq_no:{0}>3}" for seq_no in self.stream_no]
+        self.registry    = [None for _ in self.stream_no]
+
+        for key in predefined_components:
+            self.registry[predefined_components[key]] = key  
+
+    def _register_component(self, component_name, component_type):
+        unassigned_idx = [i for i, val in enumerate(self.registry) if val == None]
+        unassigned_stream = self.stream_no[unassigned_idx]
+        self.registry[jnp.min(unassigned_stream[unassigned_stream >= 128])] = component_name.lower()
+
+    def setup_stream(self, component_name, **kwargs):
+        if component_name in self.registry:
+            stream_no = self.registry.index(component_name)
+            return RNG_component(component_name, self.stream_id[stream_no], self.key_list[stream_no], **kwargs)
+    
+
--- xgmutil/stream.py
+++ xgmutil/stream.py
@@ -1,7 +1,6 @@
 import jax.numpy as jnp 
 import jax.random as rnd
 import jax
-import sys
 import os
 
 class Stream:
@@ -9,18 +8,26 @@ class Stream:
     def __init__(self, **kwargs):
 
         self.force_no_gpu = kwargs.get('force_no_gpu',False)
-        self.seed         = kwargs.get('seed',123456789)
+        self.seedkey      = kwargs.get('seedkey', rnd.PRNGKey(123456789))
         self.nsub         = kwargs.get('nsub',1024**3)
 
     def generate(self,**kwargs):
 
         if self.force_no_gpu:
-            _JAX_PLATFORM_NAME = os.environ["JAX_PLATFORM_NAME"]
-            os.environ["JAX_PLATFORM_NAME"] = "cpu"
+            _JAX_PLATFORM_NAME = jax.default_backend()
+            jax.default_device("cpu")
 
         start = kwargs.get('start',0)
         size  = kwargs.get('size' ,1)
         dist  = kwargs.get('dist','normal')
+        dtype = kwargs.get('dtype', jnp.float32)
+        mc    = kwargs.get('mc', 0)
+
+        if dtype in [jnp.float64, jnp.complex128, jnp.int64]:
+            _JAX_X64_INITIAL_STATE = jax.config.read('jax_enable_x64')
+            jax.config.update('jax_enable_x64', True)
+
+        key4mc = rnd.fold_in(self.seedkey, mc)
 
         end = start + size - 1
 
@@ -29,9 +36,7 @@ class Stream:
 
         seqIDs = jnp.arange(start_seqID, end_seqID+1)
 
-        seedkey = rnd.PRNGKey(self.seed)
-
-        keys = jax.vmap(rnd.fold_in, in_axes=(None, 0), out_axes=0)(seedkey, seqIDs)
+        keys = jax.vmap(rnd.fold_in, in_axes=(None, 0), out_axes=0)(key4mc, seqIDs)
 
         seq = jnp.zeros(0,dtype=jnp.float32)
 
@@ -40,12 +45,16 @@ class Stream:
             subseq_start = max(seqID * self.nsub, start) - seqID * self.nsub
             subseq_end   = min((seqID+1) * self.nsub -1,end) - seqID * self.nsub
 
-            subseq = rnd.normal(keys[seqID], dtype=jnp.float32, shape=(self.nsub,))
+            if dist == 'normal':
+                subseq = rnd.normal(keys[seqID], dtype=dtype, shape=(self.nsub,))
 
             seq = jnp.concatenate((seq,subseq[subseq_start:subseq_end+1]))
         
         if self.force_no_gpu:
-            os.environ["JAX_PLATFORM_NAME"] = _JAX_PLATFORM_NAME
+            jax.default_device(_JAX_PLATFORM_NAME)
+
+        if dtype in [jnp.float64, jnp.complex128, jnp.int64]:
+            jax.config.update('jax_enable_x64', _JAX_X64_INITIAL_STATE)
 
         return seq
 
